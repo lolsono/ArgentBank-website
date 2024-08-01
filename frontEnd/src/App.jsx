f843b024cb668f12a61e2088e942a9ac613466bd
lolsono <155098365+lolsono@users.noreply.github.com>
2024-08-01 13:36:10 +0200

    ajout du style et ajout de la page home
    -transformation de la page index en home.jsx pour intégrations

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,10 +1,14 @@
 import Routing from "./Routes/Routes.jsx";
+import Footer from "./components/Footer.jsx";
+import Navbar from "./components/NavBar.jsx";
 
 function App() {
   return (
     <>
       <main>
+        <Navbar />
         <Routing />
+        <Footer />
       </main>
     </>
   );
