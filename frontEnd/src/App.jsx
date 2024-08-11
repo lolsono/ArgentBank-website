986dec21bbe7a21f5ef88ecc2de6cb7d787de530
lolsono <155098365+lolsono@users.noreply.github.com>
2024-08-11 19:35:06 +0200

    réglage du style de la page de connexion
    -ajout du main personaliser selon la page du site web

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,12 +1,19 @@
+/* eslint-disable no-unused-vars */
+// App.js
+import React from "react";
+import { useLocation } from "react-router-dom";
 import Routing from "./Routes/Routes.jsx";
 import Footer from "./components/Footer.jsx";
 import Navbar from "./components/NavBar.jsx";
 
 function App() {
+  const location = useLocation();
+  const isSignInPage = location.pathname === "/signIn";
+
   return (
     <>
       <Navbar />
-      <main>
+      <main className={isSignInPage ? "main-signIn" : ""}>
         <Routing />
       </main>
       <Footer />
--- frontEnd/src/Pages/SignIn.jsx
+++ frontEnd/src/Pages/SignIn.jsx
@@ -3,27 +3,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function SignIn() {
   return (
-    <div className="signin-containner bg-dark">
-      <section className="sign-in-content">
-        <FontAwesomeIcon icon={faCircleUser} />
-        <h1>Sign In</h1>
-        <form>
-          <div className="input-wrapper">
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
-          </div>
-          <div className="input-wrapper">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
-          </div>
-          <div className="input-remember">
-            <input type="checkbox" id="remember-me" />
-            <label htmlFor="remember-me">Remember me</label>
-          </div>
+    <div className="sign-in-content">
+      <FontAwesomeIcon icon={faCircleUser} />
+      <h1>Sign In</h1>
+      <form>
+        <div className="input-wrapper">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" />
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div className="input-remember">
+          <input type="checkbox" id="remember-me" />
+          <label htmlFor="remember-me">Remember me</label>
+        </div>
 
-          <button className="sign-in-button">Sign In</button>
-        </form>
-      </section>
+        <button className="sign-in-button">Sign In</button>
+      </form>
     </div>
   );
 }
